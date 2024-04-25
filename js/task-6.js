fc9575c353dac1cba68761bf22826f2a31b3646b
AnaPuris <apuris911@gmail.com>
2024-04-25 14:31:14 +0100

    hw7-vers4

--- js/task-6.js
+++ js/task-6.js
@@ -10,15 +10,19 @@ function createBoxes(amount) {
   const boxesContainer = document.querySelector('#boxes');
   let size = 30;
 
+  const fragment = document.createDocumentFragment();
+
   for (let i = 0; i < amount; i++){
     const box = document.createElement('div');
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+    fragment.appendChild(box);
     size = size + 10;
   }
 }
+
+boxesContainer.appendChild(fragment);
 
 function destroyBoxes() {
   const boxesContainer = document.querySelector('#boxes');
